b3ee28315058d2e38056e1c9f66aec96d989621c
Korkia <korkija@gmail.com>
2020-05-06 11:45:48 +0300

    upgrade

--- index.js
+++ index.js
@@ -1,89 +1,118 @@
-
-const available = [240,360,720];
-const allowed = [360,720];
-const preferred = [1080];
+const outputs = [
+    [720],
+    [720],
+    [],
+    [240, 360],
+    [240, 720],
+    [240],
+    [],
+    [360],
+    [360, 720],
+    [240, 360, 720],
+    [360],
+    [],
+]
 
 const inputs = [
-    {available:[240,360,720],
-        allowed:[360,720],
-        preferred:[1080],
+    {
+        available: [240, 360, 720],
+        allowed: [360, 720],
+        preferred: [1080],
     },
-    // {available:[240,360,720],
-    //     allowed:[360,720],
-    //     preferred:[360],
-    // },
-    {available:[240,720],
-        allowed:[360,720],
-        preferred:[1080],
-        // preferred:[240,720,1080],
+    {
+        available: [240, 720],
+        allowed: [360, 720],
+        preferred: [1080],
     },
 
-    {available:[240],
-        allowed:[360,720],
-        preferred:[1080],
+    {
+        available: [240],
+        allowed: [360, 720],
+        preferred: [1080],
     },
-    {available:[240,360,720],
-        allowed:[240,360,720,1080],
-        preferred:[240,360],
+    {
+        available: [240, 360, 720],
+        allowed: [240, 360, 720, 1080],
+        preferred: [240, 360],
     },
-    {available:[240,720],
-        allowed:[240,360,720,1080],
-        preferred:[240,360],
+    {
+        available: [240, 720],
+        allowed: [240, 360, 720, 1080],
+        preferred: [240, 360],
     },
-    {available:[240,720],
-        allowed:[240,360,1080],
-        preferred:[240,360],
+    {
+        available: [240, 720],
+        allowed: [240, 360, 1080],
+        preferred: [240, 360],
     },
-    {available:[720],
-        allowed:[240,360,1080],
-        preferred:[240,360],
+    {
+        available: [720],
+        allowed: [240, 360, 1080],
+        preferred: [240, 360],
     },
-    {available:[240,360],
-        allowed:[240,360],
-        preferred:[720,1080],
+    {
+        available: [240, 360],
+        allowed: [240, 360],
+        preferred: [720, 1080],
     },
-    {available:[240,360,720],
-        allowed:[360,'any'],
-        preferred:[360,720],
+    {
+        available: [240, 360, 720],
+        allowed: [360, 'any'],
+        preferred: [360, 720],
     },
-    {available:[240,360,720],
-        allowed:[240,360,720],
-        preferred:['any',720],
+    {
+        available: [240, 360, 720],
+        allowed: [240, 360, 720],
+        preferred: ['any', 720],
     },
-    {available:[240,360,720],
-        allowed:[360,1080],
-        preferred:['any',720],
+    {
+        available: [240, 360, 720],
+        allowed: [360, 1080],
+        preferred: ['any', 720],
     },
-    {available:[240,360,720],
-        allowed:[1080],
-        preferred:['any',720],
+    {
+        available: [240, 360, 720],
+        allowed: [1080],
+        preferred: ['any', 720],
     },
 ]
 
-attempt = (available, allowed, preferred)=>{
+attempt = (available, allowed, preferred) => {
+    const allowedSet = new Set(allowed);
     const availAllow =
-        allowed.find(item => item ==='any')
+        allowed.includes('any')
             ?
-            available:
-            available.filter((item)=>allowed.includes(item,0));
+            available :
+            available.filter((item) => allowedSet.has(item));
+
     if (!!availAllow.length) {
-        if (preferred.find(item => item ==='any'))
+        if (preferred.includes('any')) {
             return availAllow;
+        }
+
+        const availAllowReverse = [...availAllow].reverse();
+
         const result = preferred.reduce((accum, itemP) => {
             const max = availAllow.find(itemAA => itemAA >= itemP);
-            const min = availAllow.reverse().find(itemAA => itemAA < itemP);
-            availAllow.reverse();
-            return accum.add(max ? max : min);
+
+            if (max) {
+                return accum.add(max);
+            }
+
+            const min = availAllowReverse.find(itemAA => itemAA < itemP);
+
+            return accum.add(min);
         }, new Set([]));
+
         return Array.from(result);
     }
     return [];
 }
 
-window.onload = ()=>{
-    inputs.forEach((item,index)=> {
-            console.log(`case ${index+1}`);
-            console.log(attempt(item.available, item.allowed, item.preferred));
-        }
-    )
-}
+inputs.forEach((item, index) => {
+        console.log(`case ${index}`);
+        const result = attempt(item.available, item.allowed, item.preferred);
+        console.log(result, JSON.stringify(result) === JSON.stringify(outputs[index]));
+    }
+)
+
